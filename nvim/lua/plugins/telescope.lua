b1969f4e90681a2a9b836241bf79cc5a203f0da1
jvdende <jvdende@users.noreply.github.com>
2024-06-07 00:22:32 +0200

    nvim: add telescope-fzf-native.nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
   {
     "nvim-telescope/telescope.nvim", branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
     keys = {
       { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
       { "<C-f>", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
@@ -26,7 +29,11 @@ return {
           },
         }
       },
-    }
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("fzf")
+    end,
   },
   {
     "nvim-telescope/telescope-ui-select.nvim",
