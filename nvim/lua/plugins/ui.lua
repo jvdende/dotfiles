d5d71557217ef37a3ce961cc58891aa25d8cc340
jvdende <jvdende@users.noreply.github.com>
2024-06-06 17:28:18 +0200

    nvim: update bufferline
    use nvim_lsp diagnostics and don't show close icons for buffers

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -15,10 +15,11 @@ return {
   {
     'akinsho/bufferline.nvim', version = "*",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    enabled = true,
     opts = {
       options = {
         numbers = "buffer_id",
+        diagnostics = "nvim_lsp",
+        show_buffer_close_icons = false,
       }
     }
   },
