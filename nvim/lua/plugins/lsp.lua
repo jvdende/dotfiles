6132f8cc0fb97c4eae1ae10f12ae2afd1ee472ef
jvdende <jvdende@users.noreply.github.com>
2024-07-10 16:08:47 +0200

    nvim: create floating window for lsp diagnostic

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -114,17 +114,20 @@ return {
         })
       })
 
-      vim.diagnostic.config({
-        -- update_in_insert = true,
-        float = {
-          focusable = false,
-          style = "minimal",
-          border = "rounded",
-          source = "always",
-          header = "",
-          prefix = "",
-        },
+      vim.api.nvim_create_autocmd("CursorHold", {
+        buffer = bufnr,
+        callback = function()
+          local opts = {
+            focusable = false,
+            close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+            border = "rounded",
+            source = "always",
+            prefix = " ",
+            scope = "cursor",
+          }
+          vim.diagnostic.open_float(nil, opts)
+        end
       })
     end
-  }
+  },
 }
