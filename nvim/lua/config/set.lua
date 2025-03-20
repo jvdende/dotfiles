2612f7a80a0897f84b6eb08e74fa2847ae74d474
jvdende <jvdende@users.noreply.github.com>
2025-03-20 15:02:33 +0100

    feat(nvim): remove windows check for wsl clipboard
    Since it didn't work anyway, removing it for now

--- nvim/lua/config/set.lua
+++ nvim/lua/config/set.lua
@@ -24,17 +24,15 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- Wsl Clipboard
-if vim.fn.has('win32') then
-  vim.g.clipboard = {
-    name = 'WslClipboard',
-    copy = {
-      ['+'] = 'clip.exe',
-      ['*'] = 'clip.exe',
-    },
-    paste = {
-      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
-end
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
